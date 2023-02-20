a13046271fb83cb3e1680f74fd288e84cde2907d
Stackbit Code Editor <projects@stackbit.com>
2023-02-20 18:02:38 +0000

    stackbit.config.ts: changed

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -125,7 +125,7 @@ const config = defineStackbitConfig({
                             urlPath: document.fields.slug.value,
                             label: document.fields.title.value,
                             stableId: document.srcObjectId,
-                            locale: ''
+                            locale: document.fields.locale.value
                         }
                                     case 'PostLayout':
                     return {
