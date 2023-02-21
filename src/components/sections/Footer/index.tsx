04c54e682524dcb553bb877c961492664598f91e
Stackbit Code Editor <projects@stackbit.com>
2023-02-21 18:17:06 +0000

    index.tsx: changed

--- src/components/sections/Footer/index.tsx
+++ src/components/sections/Footer/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import Markdown from 'markdown-to-jsx';
 import classNames from 'classnames';
 
@@ -82,8 +82,8 @@ export default function Footer(props) {
                                                 value={router.locale}
                                                 onChange={(e) => {
                                                     const locale = e.target.value;
-                                                    router.push({ pathname: router.pathname, query: router.query }, router.asPath, { locale });
                                                     (window as any).stackbit?.setLocale(locale);
+                                                    router.push({ pathname: router.pathname, query: router.query }, router.asPath, { locale });
                                                 }}
                                                 className="block appearance-none w-full bg-white text-black border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                                             >
